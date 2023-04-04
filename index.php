da9e63eee33527553fc7b3115d19330ac434e66e
Iloonio <decadentlithium@gmail.com>
2023-04-04 18:44:17 +0200

    fuck me man

--- index.php
+++ index.php
@@ -1,4 +1,42 @@
 <?php
+session_start();
+
+  // Set this to true if login is successful
+  $loggedIn = false;
+
+  // Check if login form is submitted
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Check if username and password are correct
+    if ($username == "myusername" && $password == "mypassword") {
+      // Set the session variable
+      $_SESSION["loggedIn"] = true;
+      $_SESSION["username"] = $username;
+      $loggedIn = true;
+    } else {
+      // Invalid username or password
+      echo "Invalid username or password";
+    }
+  }
 
 include("includes/header.php");
+
+if ($loggedIn) { ?>
+    <p>Welcome, <?php echo $_SESSION["username"]; ?>!</p>
+  <?php } else { ?>
+    <form action="index.php" method="post">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="myusername">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" value="mypassword">
+      </div>
+      <input type="submit" value="Log in">
+    </form>
+  <?php } ?>
+
 include("includes/footer.php");
