1461fb80eee9944a6d828e93a3ede10317376e3d
Iloonio <decadentlithium@gmail.com>
2023-04-04 21:13:44 +0200

    Revert "only the css left!"
    This reverts commit b3001a760ae4494a63f6a01ce323961d1452bb26.

--- includes/navigation_list.php
+++ includes/navigation_list.php
@@ -1,5 +1,6 @@
 <ul class="navigation">
-        <li><a href="startmenu.php">Home</a></li>
+        <li><a href="#">Home</a></li>
         <li><a href="information.php">Information</a></li>
+        <li><a href="website.php">Startmenu</a></li>
         <li><a href="includes/logout.php">Logout</a></li>
       </ul>
