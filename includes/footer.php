1942aa4ce0b20d74a51689ac6758a6ff90d853b3
Iloonio <decadentlithium@gmail.com>
2023-04-04 18:13:39 +0200

    Doing more

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+</div>
+    <div class="footer">
+      <p>Kurskod: DT100G</p>
+      <p>Ruslan Abdulin</p>
+      <p>ruab2100@student.miun.se</p>
+    </div>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Laboration 3 - Webbprogrammering DT100G</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+  <div class="container">
+      <h1>Laboration 3 - Webbprogrammering DT100G</h1>
+      <ul class="navigation">
+        <li><a href="#">Home</a></li>
+        <li><a href="information.php">Information</a></li>
+        <li><a href="website.php">Startmenu</a></li>
+      </ul>
